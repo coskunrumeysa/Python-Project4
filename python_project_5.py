b0a63d877b46978770e63e74bb66d8c1aae7e850
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2023-01-20 11:11:12 +0300

    Add files via upload

---
+++ python_project_5.py
@@ -0,0 +1,31 @@
+# İntroduce who witten this program.
+__author__ = "Rümeysa Coskun - Computer && Software Engineer"
+
+ # This program written for escape sequence .
+
+# 1- \\ : use for  \ symbol.
+print("This program written for escape sequence in 20\\01 \\2023")
+
+# 2- \t : use for break empty a tab.
+print("This program written for escape sequence :\t 20\\01\\2023")
+
+# 3- \r : use for before of yourself delete.
+print("This program written for escape sequence \r:\t\t 20\\01\\2023 ")
+
+# 4-\" : use for " symbol.
+print("This program written for \" Escape Sequence\"")
+
+# 5- \' :use for ' symbol .
+print("This program written for \'escape sequence.\'")
+
+# 6- \n :  use for access new line.
+print("This program written for \n Escape sequence \t 20\\01\\2023")
+
+# 7- \a : use for bip tone.
+print("This program written for : \n \"Escape Sequence\" \a ")
+
+# 8- \b :
+print("This program written for:\bEscape Sequence")
+
+# 9- \v :
+print("This program written for:\vEscape Sequence")
